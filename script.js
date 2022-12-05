dd49e3f99a5438058789dd884d0b624540ec5465
Silkoway <ethancai920@gmail.com>
2022-12-05 12:01:04 +0800

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+let y = document.getElementById("yoshi")
+let h = document.getElementById("hand");
+let count = 0;
+let c = document.getElementById("count");
+
+document.addEventListener("click", () => {
+    y.classList.add("hit");
+    let r = Math.random();
+    let q = r < 0.1;
+    if (q) {
+        y.setAttribute("src", "yoshiblock.png")
+    } else {
+        y.setAttribute("src", "sadyoshi.webp")
+        count++;
+        c.textContent = "You have hit yoshi " + count + " times"
+        h.classList.add("hit");
+    }
+    
+    setTimeout(() => {
+        y.classList.remove("hit");
+        h.classList.remove("hit")
+        y.setAttribute("src", "yoshi.png")
+    }, 100)
+})
+
+document.addEventListener("mousemove", (e) => {
+    h.style.top = e.clientY + "px"
+})
